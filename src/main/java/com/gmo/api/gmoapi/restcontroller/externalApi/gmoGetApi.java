9c20085aeb61a0b8ec2937b943633651f9e81960
CattyMYuki <yuki.m.0308@gmail.com>
2021-07-08 02:49:02 +0000

    update

--- src/main/java/com/gmo/api/gmoapi/restcontroller/externalApi/gmoGetApi.java
+++ src/main/java/com/gmo/api/gmoapi/restcontroller/externalApi/gmoGetApi.java
@@ -1,6 +1,34 @@
 package com.gmo.api.gmoapi.restcontroller.externalApi;
 
-public class gmoGetApi {
+import com.google.gson.Gson;
+import okhttp3.*;
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.util.*;
 
-    
+public class gmoGetApi {
+    private String url = "https://api.coin.z.com/public/v1/ticker";
+    private HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder();
+
+    public String getTicker(String alt_coin) throws Exception {
+        Gson gson = new Gson();
+
+        // Add request Param
+        Map<String, String> params = new HashMap<>();
+        params.put("symbol", alt_coin);
+        params.forEach(urlBuilder::addQueryParameter);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(urlBuilder.build()).build();
+
+        //HTTP Request 
+        Call call = client.newCall(request);
+        Response response = call.execute();
+
+        //Parse JSON format
+        // String result = gson.fromJson(response.body().string(), String.class);
+        String result = response.body().string();
+        return result;
+    }
+
 }
--- src/main/java/com/gmo/api/gmoapi/restcontroller/restApiComponent.java
+++ src/main/java/com/gmo/api/gmoapi/restcontroller/restApiComponent.java
@@ -1,8 +1,11 @@
 package com.gmo.api.gmoapi.restcontroller;
+import com.gmo.api.gmoapi.restcontroller.externalApi.gmoGetApi;
 //import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class restApiComponent {	
@@ -10,4 +13,9 @@ public class restApiComponent {
 	 public String hello(){
 		 return "hello";
 	 } 
-}
+	@GetMapping("/ticker")
+	 public String ticker(@RequestParam("alt_coin") String alt_coin) throws Exception {
+		gmoGetApi gmoGetApi = new gmoGetApi();
+		return gmoGetApi.getTicker(alt_coin);
+	 }
+	}
