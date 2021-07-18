916c2a97c3ae7bfd15cfd6997fc621c6921556b3
CattyMYuki <yuki.m.0308@gmail.com>
2021-07-18 14:47:53 +0000

    test

--- src/main/java/com/gmo/api/gmoapi/restcontroller/restApiComponent.java
+++ src/main/java/com/gmo/api/gmoapi/restcontroller/restApiComponent.java
@@ -2,6 +2,7 @@ package com.gmo.api.gmoapi.restcontroller;
 import com.gmo.api.gmoapi.restcontroller.externalApi.gmoGetApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ch.qos.logback.core.rolling.helper.MonoTypedConverter;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -10,6 +11,10 @@ public class restApiComponent {
 	 public String hello(){
 		 return "hello";
 	 } 
+	@GetMapping("/")
+    Mono bntest_mono(){
+
+	}
 	@GetMapping("/ticker")
 	 public String ticker(@RequestParam("alt_coin") String alt_coin) throws Exception {
 		gmoGetApi gmoGetApi = new gmoGetApi();
